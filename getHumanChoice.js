aafaa2da557ee12ab4a2190077c24b062d3aa01d
Walkeron-ua <177946022+Walkeron-ua@users.noreply.github.com>
2024-09-24 17:28:26 +0300

    Unite getHumanChoice.js & getComputerChoice.js
    Rename it into javascript.js

--- getHumanChoice.js
+++
@@ -1,4 +0,0 @@
-function getHumanChoice() {
-    let choice = prompt("Type one of three value: Rock, Paper or Scissors");
-    return choice[0].toUpperCase() + choice.slice(1).toLowerCase();
-}
--- javascript.js
+++ javascript.js
@@ -8,3 +8,8 @@ function getComputerChoice() {
         return 'Scissors';
     }
 }
+
+function getHumanChoice() {
+    let choice = prompt("Type one of three value: Rock, Paper or Scissors");
+    return choice[0].toUpperCase() + choice.slice(1).toLowerCase();
+}
