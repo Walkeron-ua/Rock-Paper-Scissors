4ce9a9a19c62c5aa07af530e67bc84da55586585
Walkeron-ua <177946022+Walkeron-ua@users.noreply.github.com>
2024-09-24 18:12:41 +0300

    Add new function playRound()
    Create two variables humanScore & computerScore
    Create two constants humanChoice & computerChoice which store value
    from getHumanChoice() & getComputerChoice() functions
    Function playRound() take two parameters humanChoice & computerChoice
    and return some text in instance : “You lose! Paper beats Rock”
    and human's and computer's score.

--- javascript.js
+++ javascript.js
@@ -1,3 +1,9 @@
+let humanScore = 0;
+let computerScore = 0;
+
+const humanChoice = getHumanChoice();
+const computerChoice = getComputerChoice();
+
 function getComputerChoice() {
     let randomNumber = Math.floor(Math.random() * 3); 
     if  (randomNumber == 0) {
@@ -13,3 +19,31 @@ function getHumanChoice() {
     let choice = prompt("Type one of three value: Rock, Paper or Scissors");
     return choice[0].toUpperCase() + choice.slice(1).toLowerCase();
 }
+
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice == "Paper" & computerChoice =="Rock") {
+        ++humanScore;
+        return console.log(`You Win! Paper beats Rock.\n\nYour Score: ${humanScore} | Computer Score: ${computerScore}`);
+    }else if (humanChoice == "Paper" & computerChoice =="Scissors") {
+        ++computerScore;
+        return console.log(`You Lose! Scissors beats Paper.\n\nYour Score: ${humanScore} | Computer Score: ${computerScore}`);
+    }else if (humanChoice == "Rock" & computerChoice =="Paper") {
+        ++computerScore;
+        return console.log(`You Lose! Paper beats Rock.\n\nYour Score: ${humanScore} | Computer Score: ${computerScore}`);
+    }else if (humanChoice == "Rock" & computerChoice =="Scissors") {
+        ++humanScore;
+        return console.log(`You Win! Rock beats Scissors.\n\nYour Score: ${humanScore} | Computer Score: ${computerScore}`);
+    }else if (humanChoice == "Scissors" & computerChoice =="Paper") {
+        ++humanScore;
+        return console.log(`You Win! Scissors beats Paper.\n\nYour Score: ${humanScore} | Computer Score: ${computerScore}`);
+    }else if (humanChoice == "Scissors" & computerChoice =="Rock") {
+        ++computerScore;
+        return console.log(`You Lose! Rock beats Scissors.\n\nYour Score: ${humanScore} | Computer Score: ${computerScore}`);
+    }else {
+        ++humanScore;
+        ++computerScore;
+        return console.log(`WoW! You both picked the same.\n\nYour Score: ${humanScore} | Computer Score: ${computerScore}`);
+    }
+}
+
+playRound(humanChoice, computerChoice);
